f23c10ecaab0c6e76b1b0f534c3762d548ef79c7
Xu Quan <xuquan1225@hotmail.com>
2025-03-02 20:42:23 +0800

    refactor: update drag item structure to support future multiple files

--- src/hooks/useDraggable.tsx
+++ src/hooks/useDraggable.tsx
@@ -1,10 +1,29 @@
+import { TFile, TFolder } from "obsidian";
 import { CSSProperties, useEffect } from "react";
 import { useDrag } from "react-dnd";
 import { getEmptyImage } from "react-dnd-html5-backend";
 
+import { FFS_DRAG_FILE_TYPE, FFS_DRAG_FOLDER_TYPE } from "src/assets/constants";
+
+export type DraggableFile = {
+	file: TFile;
+};
+export type DraggableFolder = {
+	folder: TFolder;
+};
+
+export const isDraggableFile = (item: DraggableItem): item is DraggableFile =>
+	(item as DraggableFile).file !== undefined;
+
+export const isDraggableFolder = (
+	item: DraggableItem
+): item is DraggableFolder => (item as DraggableFolder).folder !== undefined;
+
+export type DraggableItem = DraggableFile | DraggableFolder;
+
 type Props = {
-	type: string;
-	item: unknown;
+	type: typeof FFS_DRAG_FILE_TYPE | typeof FFS_DRAG_FOLDER_TYPE;
+	item: DraggableItem;
 };
 const useDraggable = ({ type, item }: Props) => {
 	const [{ isDragging }, drag, preview] = useDrag(() => ({
