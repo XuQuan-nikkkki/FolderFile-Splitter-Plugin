95d10fa1316a8a0ebf235c7e191ccc00e021475c
Xu Quan <xuquan1225@hotmail.com>
2025-03-07 10:37:04 +0800

    refactor: seperate file content and drag file logic

--- src/components/File.tsx
+++ src/components/File.tsx
@@ -7,40 +7,21 @@ import FolderFileSplitterPlugin from "src/main";
 import { FolderListModal } from "./FolderListModal";
 import { useShowFileDetail } from "src/hooks/useSettingsHandler";
 import FileDetail from "./FileDetail";
-import useDraggable, { getDraggingStyles } from "src/hooks/useDraggable";
-import { FFS_DRAG_FILES_TYPE } from "src/assets/constants";
-import { isAbstractFileIncluded } from "src/utils";
 import useRenderFileName from "src/hooks/useRenderFileName";
 
 type Props = {
 	useFileTreeStore: UseBoundStore<StoreApi<FileTreeStore>>;
 	file: TFile;
-	fileList: TFile[];
 	plugin: FolderFileSplitterPlugin;
 	deleteFile: () => void;
-	selectedFiles: TFile[];
-	draggingFiles: TFile[];
-	setSelectedFiles: (files: TFile[]) => void;
-	setDraggingFiles: (files: TFile[]) => void;
 };
-const File = ({
-	file,
-	fileList,
-	useFileTreeStore,
-	plugin,
-	deleteFile,
-	selectedFiles,
-	setSelectedFiles,
-	draggingFiles,
-	setDraggingFiles,
-}: Props) => {
+const File = ({ file, useFileTreeStore, plugin, deleteFile }: Props) => {
 	const {
 		focusedFile,
 		selectFile,
 		createFile,
 		duplicateFile,
 		folders,
-		setFocusedFile,
 		isFilePinned,
 		pinFile,
 		unpinFile,
@@ -51,25 +32,11 @@ const File = ({
 			createFile: store.createFile,
 			duplicateFile: store.duplicateFile,
 			folders: store.folders,
-			setFocusedFile: store.setFocusedFile,
 			isFilePinned: store.isFilePinned,
 			pinFile: store.pinFile,
 			unpinFile: store.unpinFile,
 		}))
 	);
-
-	const { drag, isDragging } = useDraggable({
-		type: FFS_DRAG_FILES_TYPE,
-		item: () => {
-			const filesToDrag = isFileSelected() ? selectedFiles : [file];
-			setDraggingFiles(filesToDrag);
-			return { files: filesToDrag };
-		},
-		end: () => {
-			setDraggingFiles([]);
-		},
-		deps: [selectedFiles, draggingFiles],
-	});
 
 	const { showFileDetail } = useShowFileDetail(
 		plugin.settings.showFileDetail
@@ -150,80 +117,17 @@ const File = ({
 		return <FileDetail useFileTreeStore={useFileTreeStore} file={file} />;
 	};
 
-	const _isFileSelected = (fi: TFile) =>
-		isAbstractFileIncluded(selectedFiles, fi);
-
-	const isFileSelected = () => _isFileSelected(file);
-
-	const beginMultiSelect = (): TFile[] => {
-		setFocusedFile(null);
-		let newFiles = [...selectedFiles];
-		if (focusedFile && !_isFileSelected(focusedFile)) {
-			newFiles = [...selectedFiles, focusedFile];
-		}
-		return newFiles;
-	};
-
-	const onSelectRange = () => {
-		const files = beginMultiSelect();
-		if (!files.length) return;
-		const lastSelectedFile = files[files.length - 1];
-		const lastIndex = fileList.findIndex(
-			(f) => f.path === lastSelectedFile?.path
-		);
-		const currentIndex = fileList.findIndex((f) => f.path === file.path);
-
-		const [start, end] =
-			lastIndex < currentIndex
-				? [lastIndex, currentIndex]
-				: [currentIndex, lastIndex];
-
-		const newFiles = fileList.slice(start, end + 1);
-		setSelectedFiles(newFiles);
-	};
-
-	const onSelectOneByOne = () => {
-		let files = beginMultiSelect();
-		if (isFileSelected()) {
-			files = files.filter((f) => f.path !== file.path);
-		} else {
-			files.push(file);
-		}
-		setSelectedFiles(files);
-	};
-
-	const onClickFile = (e: React.MouseEvent<HTMLDivElement>) => {
-		if (e.shiftKey && (selectedFiles.length > 1 || focusedFile)) {
-			onSelectRange();
-		} else if (e.altKey || e.metaKey) {
-			onSelectOneByOne();
-		} else {
-			setSelectedFiles([file]);
-			selectFile(file);
-		}
-	};
-
-	const getIsDragging = () =>
-		isDragging || isAbstractFileIncluded(draggingFiles, file);
-
 	const getFileClassName = () => {
-		const isFocused = focusedFile?.path === file.path;
-		return [
-			"ffs-file",
-			isFocused && "ffs-focused-file",
-			isFileSelected() && "ffs-selected-file",
-		]
-			.filter(Boolean)
-			.join(" ");
+		return ["ffs-file", isFocused && "ffs-focused-file"]
+		.filter(Boolean)
+		.join(" ");
 	};
-
+	
+	const isFocused = focusedFile?.path === file.path;
 	return (
 		<div
-			ref={drag}
-			className={getFileClassName()}
-			onClick={onClickFile}
+			className={isFocused ? "ffs-focused-file" : ""}
 			onContextMenu={onShowContextMenu}
-			style={getDraggingStyles(getIsDragging())}
 		>
 			<div className="ffs-file-content">
 				{renderFileName()}
