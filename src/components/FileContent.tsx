c53cd3e96865a3c43b58d91588d9a7ad600918dc
Xu Quan <xuquan1225@hotmail.com>
2025-03-14 17:25:10 +0800

    refactor: define StyledFile in dependent file

--- src/components/FileContent.tsx
+++ src/components/FileContent.tsx
@@ -9,39 +9,7 @@ import FileDetail from "./FileDetail";
 import useRenderFileName from "src/hooks/useRenderFileName";
 import { useFileTree } from "./FileTree";
 import { useEffect, useRef, useState } from "react";
-
-const StyledFileContent = styled.div<{
-	$isFocused: boolean;
-	$isLast: boolean;
-	$disableGap?: boolean;
-}>`
-	display: grid;
-	grid-template-rows: auto auto;
-	gap: ${({ $disableGap }) => ($disableGap ? undefined : "4px")};
-	border-radius: var(--ffs-border-radius);
-	padding: 12px 16px 14px;
-	background-color: ${({ $isFocused }) =>
-		$isFocused ? "var(--interactive-accent)" : undefined} !important;
-
-	&:hover {
-		background-color: var(--interactive-hover);
-	}
-
-	${({ $isFocused, $isLast }) =>
-		!$isFocused &&
-		!$isLast &&
-		`
-    &:not(:hover)::after {
-      content: "";
-      position: absolute;
-      bottom: 0;
-      left: 14px;
-      right: 12px;
-      height: var(--border-width);
-      background-color: var(--interactive-hover);
-    }
-  `}
-`;
+import { StyledFileContent } from "./Styled/StyledFile";
 
 export type FileProps = {
 	file: TFile;
