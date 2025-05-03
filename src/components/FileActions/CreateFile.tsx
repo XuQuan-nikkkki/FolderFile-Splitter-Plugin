0224b72828c5c72fc8544f7b0c4de380e34568f6
Xu Quan <xuquan1225@hotmail.com>
2025-05-03 12:02:02 +0800

    feat: disable create file when focus on tag

--- src/components/FileActions/CreateFile.tsx
+++ src/components/FileActions/CreateFile.tsx
@@ -3,21 +3,24 @@ import { useShallow } from "zustand/react/shallow";
 import { AddFileIcon } from "src/assets/icons";
 import { ExplorerStore } from "src/store";
 import { useExplorer } from "src/hooks/useExplorer";
+import classNames from "classnames";
 
 const CreateFile = () => {
 	const { useExplorerStore } = useExplorer();
 
-	const { createFile, focusedFolder, rootFolder, initOrder } =
+	const { createFile, focusedFolder, rootFolder, initOrder, focusedTag } =
 		useExplorerStore(
 			useShallow((store: ExplorerStore) => ({
 				createFile: store.createFile,
 				focusedFolder: store.focusedFolder,
 				rootFolder: store.rootFolder,
 				initOrder: store.initFilesManualSortOrder,
+				focusedTag: store.focusedTag,
 			}))
 		);
 
 	const onCreateNewFile = async () => {
+		if (focusedTag) return;
 		const targetFolder = focusedFolder || rootFolder;
 		if (!targetFolder) return;
 		await createFile(targetFolder);
@@ -26,7 +29,12 @@ const CreateFile = () => {
 
 	return (
 		<div
-			className="ffs__action-button-wrapper clickable-icon nav-action-button"
+			className={classNames(
+				"ffs__action-button-wrapper clickable-icon nav-action-button",
+				{
+					"ffs__action-button-wrapper--disabled": focusedTag,
+				}
+			)}
 			onClick={onCreateNewFile}
 		>
 			<AddFileIcon className="ffs__action-button svg-icon" />
